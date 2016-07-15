4039214c57f0f72ab105305b9b595e879e30e314
zjleon <zjleon2010@gmail.com>
2016-07-15 03:41:05 -0400

    finish gulp copy task script

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,37 @@
-const gulp = require('gulp');
-const webpackConfig = require('./configs/web.static.js')
+const gulp = require('gulp')
+const srcPath = './src'
+const tempPath = './temp'
+const distPath = './dist'
 // copy: micro service 1
 
 // https://github.com/gulpjs/gulp/blob/master/docs/recipes/running-tasks-in-series.md
 // eslint
 
-// copy: static
-// gulp.task('watch:static', () => {
-//   // place code for your default task here
-//   return gulp.src('src')
-//           .pipe(watch(static_files))
-// })
-gulp.task('webpack:static', () => {
-  return gulp.src('./src/entry/web.static.js')
+const copy = require('gulp-contrib-copy')
+gulp.task('copy:web_static', () => {
+  return gulp.src('**/*', {cwd: srcPath})
+          .pipe(copy())
+          .pipe(gulp.dest('.', {cwd: tempPath}))
+})
+
+const del = require('del')
+gulp.task('clean:web_static_temp', () => del([ tempPath + '/**/*' ]))
+gulp.task('clean:web_static_dist', () => del([ distPath + '/**/*' ]))
+
+const webpackConfig = require('./configs/webpack.web_static')
+gulp.task('webpack:web_static', () => {
+  return gulp.src('./src/entry/web_static.js', {cwd: srcPath})
           .pipe(webpackConfig)
-          .pipe(gulp.dest('dist/'))
+          .pipe(gulp.dest('./dist/'))
 })
-gulp.task('default', () => {
-  // place code for your default task here
-})
+
+gulp.task(
+  'build:web_static',
+  [
+    'clean:web_static_temp',
+    'copy:web_static',
+    'clean:web_static_dist',
+    'webpack:web_static',
+    // 'clean:web_static_temp',
+  ]
+)
