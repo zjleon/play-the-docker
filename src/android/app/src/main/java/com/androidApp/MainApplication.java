b1d445c31d8574dfb32f435ee70d430b5a422c5b
zjleon <zjleon2010@gmail.com>
2016-11-23 09:23:22 +0800

    remove ndk from local.property

--- src/android/app/src/main/java/com/androidApp/MainApplication.java
+++ src/android/app/src/main/java/com/androidApp/MainApplication.java
@@ -1,7 +1,7 @@
 package com.androidApp;
 
-import android.util.Log;
 import android.app.Application;
+import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
