ea0824c52ed681b4edeb199821188e272a1d10e3
zjleon <zjleon2010@gmail.com>
2016-11-24 01:02:44 -0500

    temp branch for merge

--- scripts/container.dev.android.initial.sh
+++ scripts/container.dev.android.initial.sh
@@ -1,5 +1,4 @@
 # node modules will be copied to /app, when container up
-cp -r /node_packages/node_modules ./node_modules
 npm install --quiet
-npm run dev:start_packager &
-gradle tasks --info
+npm run start
+# gradle tasks --info
