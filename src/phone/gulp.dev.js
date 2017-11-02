6b79eac903bf70cd8eb07e1ebf62a14f87505125
zjleon <zjleon2010@gmail.com>
2017-11-02 10:18:02 -0400

    update the logic in first image sync

--- src/phone/gulp.dev.js
+++ src/phone/gulp.dev.js
@@ -7,6 +7,7 @@ const sharp = require('sharp')
 const fs = require('fs')
 const path = require('path')
 const moment = require('moment')
+const deepequal = require('deepequal')
 
 let compiler = webpack(webpackConfig)
 let server
@@ -34,35 +35,45 @@ gulp.task('watchImages', () => {
 gulp.task('convertImages', (callback) => {
   // convert all images at the first-time gulp runs
   fs.readdir(imageFolderSrc, (error, files) => {
-    files.forEach((file) => {
-      sharpImage(path.resolve(imageFolderSrc, file))
+    let promises = files.map((file) => {
+      return sharpImage(path.resolve(imageFolderSrc, file))
+    })
+    Promise.all(promises)
+    .then(() => {
+      callback()
     })
   })
-  callback()
 })
 const sharpImage = (file) => {
   const filePath = path.parse(file)
   if (!filePath.name || !filePath.ext) {
-    return
+    return Promise.resolve()
   }
-  const distPath = path.resolve(imageFolderDist, filePath.base)
-  // check if this image exist in dist path
-  fs.stat(distPath, (error, stats) => {
-    if (error && error.code === 'ENOENT') {
-      console.log('new image detected')
-      const image = sharp(file)
-      imageMetaToJson(image, filePath.name)
-      return image.clone().toFile(distPath)
-    }
-    const originFileStat = fs.statSync(file)
-    if (moment(stats.ctime) < moment(originFileStat.mtime)) {
-      // dist image is older than original image
-      console.log('image changes detected')
-      const image = sharp(file)
-      imageMetaToJson(image, filePath.name)
-      return image.clone().toFile(distPath)
-    }
-    return true
+  return new Promise((resolve) => {
+    const distPath = path.resolve(imageFolderDist, filePath.base)
+    // check if this image exist in dist path
+    // TODO: handle the image removed event
+    fs.stat(distPath, (error, stats) => {
+      if (error && error.code === 'ENOENT') {
+        console.log('new image detected')
+        const image = sharp(file)
+        // TODO: to different screen size images, and output the url of those images
+        imageMetaToJson(image, filePath.name)
+        resolve(image.clone().toFile(distPath))
+        return
+      }
+      const originFileStat = fs.statSync(file)
+      if (moment(stats.ctime) < moment(originFileStat.mtime)) {
+        // dist image is older than original image
+        console.log('image changes detected')
+        const image = sharp(file)
+        imageMetaToJson(image, filePath.name)
+        resolve(image.clone().toFile(distPath))
+        return
+      }
+      resolve()
+      return
+    })
   })
 }
 let imageInfo = require('./common/ImageInfo.json')
@@ -73,6 +84,9 @@ const imageMetaToJson = (imagePromise, imageName) => {
       width: metadata.width,
       height: metadata.height,
       aspect: metadata.width / metadata.height,
+    }
+    if (deepequal(meta, imageInfo[imageName])) {
+      return
     }
     imageInfo[imageName] = meta
     fs.writeFile('./common/ImageInfo.json', JSON.stringify(imageInfo, null, 2), function(err) {
