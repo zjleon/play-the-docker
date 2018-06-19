8ab55dad9a2b2696cb0ce3b893bfad55ab7ef36c
Jian X Zhang <jian.x.zhang@pwc.com>
2018-06-19 17:17:57 +0800

    update route

--- services/UI/modules/Routers/index.js
+++ services/UI/modules/Routers/index.js
@@ -19,18 +19,18 @@ class RootRouter extends Component {
       <Switch>
         {routes.map((componentName, index) => {
           let DynamicComponent
-          if (NODE_ENV === 'development') {
-            DynamicComponent = require(`../${componentName}/index.js`).default
-          } else {
-            DynamicComponent = Loadable({
-              loader: () => {
-                // Catch webpack dynamic import warning
-                const component = import(`../${componentName}/index.js`).catch((error) => console.error('DynamicComponent', error))
-                return component
-              },
-              loading: Loading,
-            })
-          }
+          // if (NODE_ENV === 'development') {
+          //   DynamicComponent = require(`../${componentName}/index.js`).default
+          // } else {
+          DynamicComponent = Loadable({
+            loader: () => {
+              // Catch webpack dynamic import warning
+              const component = import(`../${componentName}/index.js`).catch((error) => console.error('DynamicComponent', error))
+              return component
+            },
+            loading: Loading,
+          })
+          // }
           return <Route
             key={`router-${index}`}
             path={`/${componentName}`}
